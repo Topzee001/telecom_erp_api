40be1e4c752e30ad900c4f2ce6960ff362509e11
Ibrahim <ibrahimsakariyah@gmail.com>
2025-08-30 12:27:24 +0100

    added urls pattersn for upload and operations api

--- telecom_erp_api/operations/urls.py
+++ telecom_erp_api/operations/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import OperationDetailsView, OperationListCreateView, OperationStatusUpdateView
 
 urlpatterns = [
-
+    path('', OperationListCreateView.as_view(), name='operations'),
+    path('details/<int:pk>/', OperationDetailsView.as_view(), name='operations-details'),
+    path('status/<int:pk>/', OperationStatusUpdateView.as_view(), name='operations-status-update')
 ]
