89a8bdea8815d6ea9e8e881e6fd4879bebb8c288
Ibrahim <ibrahimsakariyah@gmail.com>
2025-09-09 18:28:49 +0100

    bash.sh update to migrate accounts first

--- telecom_erp_api/build.sh
+++ telecom_erp_api/build.sh
@@ -5,4 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+python manage.py migrate accounts
+
+# Apply all other migrations
+python manage.py migrate --no-input
